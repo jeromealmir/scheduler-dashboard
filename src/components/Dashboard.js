18b322d4b2ec1a44a9beda43bf94de1a4f80879d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-03 00:05:35 -0700

    import loading content and define initial state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
 
 class Dashboard extends Component {
+
+  state = {
+    loading: true
+  };
+  
+
   render() {
     const dashboardClasses = classnames("dashboard");
 
