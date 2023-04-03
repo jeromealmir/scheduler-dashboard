72daf4f931763b81d0f98075f929170db6925db9
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-04-03 00:06:58 -0700

    render Loading component if loading value is true

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,10 +7,13 @@ class Dashboard extends Component {
   state = {
     loading: true
   };
-  
 
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
